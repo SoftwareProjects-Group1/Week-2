8f63c53db846e7847d1de5ce6c2e3e5d08988aba
JHMarkey <95226970+JHMarkey@users.noreply.github.com>
2022-03-15 11:26:15 +0000

    Add files via upload

---
+++ Performance Tracker/head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <script src="https://use.fontawesome.com/016ebb71ee.js"></script>
+    <title>Performance Tracker</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="site.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type = "text/css" href = "Style.css">
+    <link rel="stylesheet" type = "text/css" href = "FooterStyle.css">
+    <link rel="stylesheet" type = "text/css" href = "HeaderStyle.css">
+  </head>
---
+++ Performance Tracker/index.php
@@ -0,0 +1,12 @@
+<?php
+    require("head.php");
+    require("header.php");
+?>
+<div class="main">
+    <div class="inner_main">
+        
+    </div>
+</div> 
+<?php
+    require("footer.php");
+?>
